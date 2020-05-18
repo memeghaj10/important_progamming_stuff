07381910c17bd0089594f0b8b134eab430471d0e
Meghaj Singh <49283403+memeghaj10@users.noreply.github.com>
2020-05-19 00:04:50 +0530

    to_be_completed

---
+++ Minimum_Window_Substring.cpp
@@ -0,0 +1,71 @@
+/*
+
+This is an algorithm I thought of while trying to solve a problem during a CodeForces contest,but couldn't implement.
+
+This algorithm uses a searching string and a query string that has to be searched in the searching string, and the minimum length of the window of the searched string has to be found out!
+
+This code is yet to be completed!!!!!!!!!!!!!!!!!
+
+*/
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+	
+int minseacrhwindow(string s,string q) {
+
+	int r=0,l=0;
+
+	int h=q.length();
+
+	vector<int> v;
+
+	while(r<s.length()) {
+		int p=r;
+		for(int i=0;i<h;i++) {
+			if(count(s.begin()+l,s.begin()+r,q[i])>0) {
+				continue;
+			}
+
+			else {
+				r++;
+				break;
+			}
+		}
+
+		if(r==p) {
+			v.eb(r-l+1);
+		}
+
+
+
+	}
+
+
+}
+
+
+int main() {
+
+
+	string s;
+
+
+	cin>>s;
+
+
+	string q;
+
+
+	cin>>q;
+
+	int y=minsearchwindow(s,q);
+
+
+	cout<<y;
+
+	return 0;
+
+}	
