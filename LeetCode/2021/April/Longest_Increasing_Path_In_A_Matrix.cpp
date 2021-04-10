15485da5f26c97a4352b5f04e8a28593cca778a7
Meghaj <meghajsingh13@gmail.com>
2021-04-10 14:03:55 +0530

    added day-10

---
+++ LeetCode/2021/April/Longest_Increasing_Path_In_A_Matrix.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    
+    int dp[201][201];
+    
+    int dfs(vector<vector<int>> &matrix,int i,int j,int pre) {
+        
+            if(i<0||j<0||i==matrix.size()||j==matrix[0].size()||pre>=matrix[i][j]) {
+                return 0;
+            }
+        
+            if(dp[i][j]) {
+                return dp[i][j];
+            }
+        
+            int p=matrix[i][j];
+            matrix[i][j]=-1;
+            int l=dfs(matrix,i-1,j,p);
+            int r=dfs(matrix,i+1,j,p);
+            int u=dfs(matrix,i,j-1,p);
+            int d=dfs(matrix,i,j+1,p);
+        
+            matrix[i][j]=p;
+        
+            return dp[i][j]=max({l,r,u,d})+1;
+                
+        
+    }
+    
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        
+            memset(dp,0,sizeof(dp));
+        
+            int n=matrix.size();
+            int m=matrix[0].size();
+        
+            int ans=1;
+        
+            for(int i=0;i<n;i++) {
+                for(int j=0;j<m;j++) {
+                    ans=max(ans,dfs(matrix,i,j,-1));
+                }
+            }
+        
+        return ans;
+        
+    }
+};
