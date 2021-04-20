3f15057590527a06e4521c3c441dbd94331666b5
Meghaj <meghajsingh13@gmail.com>
2021-04-20 16:38:10 +0530

    added day-0

---
+++ LeetCode/2021/April/N-Ary_Tree_Preorder_Traversal.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:    
+    
+    void rec(Node* root) {
+        
+        ans.push_back(root->val);
+        
+        for(auto i:root->children) {
+            rec(i);
+        }
+        
+    }
+    
+    
+    vector<int> preorder(Node* root) {
+        
+        if(!root) {
+            return ans;
+        }
+        
+        rec(root);
+        return ans;
+    }
+    
+private:
+    vector<int> ans;
+};
