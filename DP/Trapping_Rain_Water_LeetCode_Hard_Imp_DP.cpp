f933594e8f58836fa938883f7dfa82d2570c265d
Meghaj <meghajsingh13@gmail.com>
2021-05-11 23:34:12 +0530

    added lc_hard_imp_trapping_rain_water_problem

---
+++ DP/Trapping_Rain_Water_LeetCode_Hard_Imp_DP.cpp
@@ -0,0 +1,65 @@
+/*
+
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+ 
+
+Example 1:
+
+
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+
+Example 2:
+
+Input: height = [4,2,0,3,2,5]
+Output: 9
+ 
+
+Constraints:
+
+n == height.length
+0 <= n <= 3 * 104
+0 <= height[i] <= 105
+
+*/
+
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        
+        int n=height.size();
+        
+        if(!n) {
+            return 0;
+        }
+        
+        vector<int> left_max(n);
+        vector<int> right_max(n);
+        
+        
+        int ans=0;
+        
+        left_max[0]=height[0];
+        right_max[n-1]=height[n-1];
+        
+        for(int i=1;i<n;i++) {
+            left_max[i]=max(height[i],left_max[i-1]);
+        }
+        
+        for(int i=n-2;i>=0;i--) {
+            right_max[i]=max(right_max[i+1],height[i]);
+        }
+        
+        for(int i=0;i<n;i++) {
+            ans+=(min(left_max[i],right_max[i])-height[i]);
+        }
+        
+        
+        return ans;
+        
+    }
+};
