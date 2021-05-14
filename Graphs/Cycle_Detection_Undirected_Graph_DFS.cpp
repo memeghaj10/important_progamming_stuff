b805241977165518257608fcb34b7bf828d50fc2
Meghaj <meghajsingh13@gmail.com>
2021-05-14 12:35:42 +0530

    added cycle_detection_using_DFS

---
+++ Graphs/Cycle_Detection_Undirected_Graph_DFS.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+
+class Solution 
+{
+    public:
+  
+    bool dfs(int node,int parent,vector<int> vis,vector<int> adj[]) {
+        
+            vis[node]=1;
+        
+            for(auto x:adj[node]) {
+                
+                if(vis[x]==0) {
+                    if(dfs(x,node,vis,adj)) {
+                        return true;
+                    }
+                }
+                
+                else if(parent!=x) {
+                    return true;
+                }
+                
+            }
+            
+            return false;
+        
+    }
+  
+  
+	bool isCycle(int V, vector<int>adj[])
+	{
+	    vector<int> vis(V,0);
+	    
+	    for(int i=0;i<V;i++) {
+	        if(vis[i]==0) {
+	            if(dfs(i,-1,vis,adj)) {
+	                return true;
+	            }
+	        }
+	    }
+	    
+	    return false;
+	    
+	}
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		Solution obj;
+		bool ans = obj.isCycle(V, adj);
+		if(ans)
+			cout << "1\n";
+		else cout << "0\n";
+	}
+	return 0;
+}  // } Driver Code Ends
