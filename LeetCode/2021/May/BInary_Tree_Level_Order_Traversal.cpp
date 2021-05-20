a1ec5fbc9f59dd678ec29412848843224683391b
Meghaj <meghajsingh13@gmail.com>
2021-05-20 14:32:21 +0530

    added day-20

--- LeetCode/2021/May/BInary_Tree_Level_Order_Traversal.cpp
+++ LeetCode/2021/May/BInary_Tree_Level_Order_Traversal.cpp
@@ -9,7 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution  {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         
