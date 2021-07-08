ed94f32eeeb18a669530591bfadfe63ed93fcc88
Meghaj <meghajsingh13@gmail.com>
2021-07-08 08:46:16 +0530

    added diagonal_traversal_of_BT

---
+++ Trees/Diagonal_Traversal_Of_Binary_Tree.cpp
@@ -0,0 +1,183 @@
+/*
+
+Given a Binary Tree, print the diagonal traversal of the binary tree.
+
+Consider lines of slope -1 passing between nodes. Given a Binary Tree, print all diagonal elements in a binary tree belonging to same line.
+
+Example 1:
+
+Input :
+            8
+         /     \
+        3      10
+      /   \      \
+     1     6     14
+         /   \   /
+        4     7 13
+Output : 8 10 14 3 6 7 13 1 4
+Explanation:
+unnamed
+Diagonal Traversal of binary tree : 
+ 8 10 14 3 6 7 13 1 4
+Your Task:
+You don't need to read input or print anything. The task is to complete the function diagonal() that takes the root node as input argumets and returns the diagonal traversal of the given tree.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(N).
+Here N is number of nodes.
+
+Constraints:
+1 <= Number of nodes<= 105
+1 <= Data of a node<= 105
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+vector<int> diagonal(Node *root);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s ,ch;
+        getline(cin, s);
+        Node* root = buildTree(s);
+
+        vector<int> diagonalNode = diagonal(root);
+        for(int i = 0;i<diagonalNode.size();i++)
+        cout<<diagonalNode[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* A binary tree node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+
+
+vector<int> diagonal(Node *root)
+{
+  
+  vector<int> ans;
+  queue<Node*> q;
+  
+  q.push(root);
+  
+  while(!q.empty()) {
+      
+      Node* r=q.front();
+      q.pop();
+      
+      while(r) {
+          
+          ans.push_back(r->data);
+          if(r->left) {
+              q.push(r->left);
+          }
+          r=r->right;
+      }
+      
+  }
+  
+  return ans;
+  
+}
