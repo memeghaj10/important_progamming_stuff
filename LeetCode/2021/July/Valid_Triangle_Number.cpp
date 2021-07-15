78ab2e0ed00ac430b02f6c4c230ec32bf851e7c2
Meghaj <meghajsingh13@gmail.com>
2021-07-15 22:55:22 +0530

    added day-15

---
+++ LeetCode/2021/July/Valid_Triangle_Number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        
+        int x=0;
+        int n=nums.size();
+        
+        sort(nums.begin(),nums.end());
+        
+        for(int i=0;i<n-2;i++) {
+            int k=i+2;
+            for(int j=i+1;j<n-1&&nums[i]!=0;j++) {
+                while(k<n&&nums[i]+nums[j]>nums[k]) {
+                    k++;
+                    x+=(k-j-1);
+                }
+            }
+        }
+        
+        return x;
+        
+    }
+};
