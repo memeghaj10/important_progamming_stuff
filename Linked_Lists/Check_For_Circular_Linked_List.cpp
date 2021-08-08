74df137b92692d0de9f7171b6ffd0bf0c9072df1
Meghaj <meghajsingh13@gmail.com>
2021-08-08 09:52:05 +0530

    added check_for_circular_LL

---
+++ Linked_Lists/Check_For_Circular_Linked_List.cpp
@@ -0,0 +1,58 @@
+/*
+
+Given head, the head of a singly linked list, find if the linked list is circular or not. A linked list is called circular if it not NULL terminated and all nodes are connected in the form of a cycle. An empty linked list is considered as circular.
+
+Example 1:
+
+Input:
+LinkedList: 1->2->3->4->5
+(the first and last node is connected,
+i.e. 5 --> 1)
+Output: 1
+Example 2:
+
+Input:
+LinkedList: 2->4->6->7->5->1
+Output: 0
+Your Task:
+The task is to complete the function isCircular() which checks if the given linked list is circular or not. It should return true or false accordingly. (the driver code prints 1 if the returned values is true, otherwise 0)
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <=Number of nodes<= 100
+
+*/
+
+/* Link list Node 
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; 
+*/
+
+/* Should return true if linked list is circular, else false */
+bool isCircular(Node *head)
+{
+   
+   Node* fixed=head;
+   Node* move=head;
+   
+   while(move) {
+       move=move->next;
+       if(move==head) {
+           return 1;
+       }
+   }
+   
+   return 0;
+   
+}
