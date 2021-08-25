f5d1b447fb8621e6721904dc605dddc9379f9716
Meghaj <meghajsingh13@gmail.com>
2021-08-25 23:42:17 +0530

    added da-25

---
+++ LeetCode/2021/August/Sum_Of_Square_Numbers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        
+     for(long long int i=0;i<=c;i++) {
+         long long int j;
+        if(i*i<=c) {
+                j= sqrt(c-i*i);
+        }
+         else {
+             break;
+         }
+         if(j*j+i*i==c) {
+             return 1;
+         }
+     }
+        
+        return 0;
+        
+    }
+};
