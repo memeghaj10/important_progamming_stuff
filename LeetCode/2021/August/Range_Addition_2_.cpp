634bc182d1d930dd5ead7e0d9310b22eee8262bb
Meghaj <meghajsingh13@gmail.com>
2021-08-30 23:32:43 +0530

    added day-30

---
+++ LeetCode/2021/August/Range_Addition_2_.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+        int min_row = m;
+        int min_col = n;
+        for (int i=0; i<ops.size(); i++){
+            if (ops[i][0]<min_row) min_row=ops[i][0];
+            if (ops[i][1]<min_col) min_col=ops[i][1];
+        }        
+        return min_row*min_col;
+        
+    }
+};
